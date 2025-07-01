000749939382815b8cebc5c874c2fc743150112f
Vinicius-Muller <viniciusmul98@gmail.com>
2025-06-30 22:57:33 -0300

    choice method created

--- Main.java
+++ Main.java
@@ -3,6 +3,6 @@ import commons.Terminal;
 public class Main {
   public static void main(String [] args) {
     Terminal vm = new Terminal();
-    vm.startTerminal();
+    vm.startTerminal("Welcome to the VM CLI");
   }
 }
--- commons/Terminal.java
+++ commons/Terminal.java
@@ -3,6 +3,9 @@ package commons;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
 
 public class Terminal {
   private final Scanner terminal;
@@ -14,8 +17,10 @@ public class Terminal {
     comparator = new Comparator();
   }
 
-  public void startTerminal() {
-    System.out.println("Welcome to the VM CLI");
+  public void startTerminal(String welcome) {
+    if(welcome != null) {
+      System.out.println(welcome);
+    }
     command = terminal.nextLine();
 
     if(command.equals("close")) {
@@ -31,18 +36,35 @@ public class Terminal {
 
       while(files.size() < 2) {
         String fileSrc = terminal.nextLine();
+
+        if(fileSrc.equals("undone")) {
+          files.remove(0);
+          System.out.println("choice undone.");
+          continue;
+        }
+
+        Path path = Paths.get(fileSrc);
+
+        if(!Files.exists(path)) {
+          System.out.println("File not found, try again.");
+          continue;
+        }
         files.add(fileSrc);
       
-        System.out.println("Please enter the to compare file path");  
+        if(files.size() == 1) {
+          System.out.println("Please enter the to compare file path");
+        }  
       }
       terminal.close();
 
       comparator.startComparation(files.get(0), files.get(1));
+      System.out.println("Diff file created into head folder");
     } else {
       System.out.println("Command not found: *Command List*");
       System.out.println("vm init | init the vm repo");
-      System.out.println("vm init | close vm cli");
-      startTerminal();
+      System.out.println("close | close vm cli");
+      System.out.println("undone | undo a choice");
+      startTerminal(null);
     }
   }
 }
